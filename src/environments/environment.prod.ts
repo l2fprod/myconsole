03ff34e418f9baa5a5bfea199794a7da05e93784
Frederic Lavigne <fred@L2FProd.com>
2016-12-23 13:57:22 +0100

    set regions for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,21 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
+  regions: [
+    {
+      id: 'ng',
+      label: 'US South',
+      flag: 'us'
+    },
+    {
+      id: 'eu-gb',
+      label: 'United Kingdom',
+      flag: 'gb'
+    },
+    {
+      id: 'au-syd',
+      label: 'Sydney',
+      flag: 'au'
+    }
+  ]
 };
